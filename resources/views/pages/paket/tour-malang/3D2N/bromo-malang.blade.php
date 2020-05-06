d5eddd487a80fd51a29b7771fae249c1037d6cf5
ramadhanqa <rafiqori@gmail.com>
2020-05-06 17:22:15 +0700

    revisi halaman detail paket

--- resources/views/pages/paket/tour-malang/3D2N/bromo-malang.blade.php
+++ resources/views/pages/paket/tour-malang/3D2N/bromo-malang.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.detail')
 @section('title')
 Tour Bromo & Malang Batu | Paket Tour Wisata Malang Bromo Murah Terbaik
 @endsection
@@ -233,61 +233,4 @@ Tour Bromo & Malang Batu | Paket Tour Wisata Malang Bromo Murah Terbaik
     </main>
 
 @endsection
-@section('about')
-<!-- About -->
-<div class="row three">
-    <div class="col-lg-7 pl-lg-5">
-      <div class="card card-instgram">
-        <h3 style="font-weight: lighter;">Our Instagram</h3>
-        <img class="rounded-circle" width="65px" height="65px" src="../../frontend/images/logoig.JPG" alt=""><p style="font-weight: lighter; padding-left: 90px; width: 1000px; margin-top: -50px; padding-bottom: 30px;">jendelawisata</p>
-        <script src="https://apps.elfsight.com/p/platform.js" defer></script>
-        <div class="elfsight-app-bbb74814-eb19-4ec3-aa2f-5541b877b9e7"></div>
-      </div>
-    </div>
-    <div class="col-lg-5">
-      <div class="card-instagram card-testimoni">
-        <h3 style="font-weight: lighter;">Testimonials</h3>
-        <div id="carouselExampleIndicators" class=" carousel jumb-testimoni slide" data-ride="carousel">
-          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="fa fa-chevron-circle-left" style="margin-left: 890px; margin-top: -425px;" ></span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
 
-            <span class="fa fa-chevron-circle-right" style="margin-top: -425px;" ></span>
-          </a>
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <p style="font-weight: lighter;">Seru banget mas, viewnya juga keren. Drivernya juga ramah dan baik, terimakasih yaa Jendela Wisata</p>
-              <br>
-                 <br>
-                <br>
-                 <br>
-                <br>
-              <strong>Indah Cahya - Jakarta</strong>
-            </div>
-            <div class="carousel-item">
-              <p style="font-weight: lighter;">Pelayanannya ramah dan berusaha memuaskan pelanggan dengan memberi lokasi setiap tmpt wisata spot yg terbaik...the best deh pokoke "duajempol" sukses selalu jendela wisata
-                <br>
-                 <br>
-                <br>
-                 <br>
-                <br>
-                </p>
-
-              <strong>Tjanaddi KFR - Tangerang</strong>
-            </div>
-            <div class="carousel-item">
-              <p style="font-weight: lighter;">Mas Hary Jon Good Service(Driver)</p>
-              <br>
-                 <br>
-                <br>
-                 <br>
-                <br>
-              <strong>Ian Oktaviani - Pekan Baru</strong>
-            </div>
-          </div>
-        </div>
-    </div>
-  </div>
-  </div>
-@endsection
