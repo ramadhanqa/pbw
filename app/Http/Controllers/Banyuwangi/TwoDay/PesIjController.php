a589cd622e4466badc3067ba38592cc3badc4989
Muhammad Hasbunallah Amril Sobri <ssobri55@yahoo.com>
2020-07-01 04:49:52 +0700

    Update Pesona Ijen

--- app/Http/Controllers/Banyuwangi/TwoDay/PesIjController.php
+++ app/Http/Controllers/Banyuwangi/TwoDay/PesIjController.php
@@ -1,10 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Banyuwangi\TwoDay;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PesIjController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        return view('pages.paket.tour-banyuwangi.2D1M.PesonaIjen');
+    }
 }
