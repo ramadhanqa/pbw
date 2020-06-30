511e24461cb1163182a21029ab0c097b2e09e466
Muhammad Hasbunallah Amril Sobri <ssobri55@yahoo.com>
2020-07-01 05:13:37 +0700

    Update Eksotiska + Keliling Banyuwangi 2

--- app/Http/Controllers/Banyuwangi/FourDay/EksoBanController.php
+++ app/Http/Controllers/Banyuwangi/FourDay/EksoBanController.php
@@ -1,10 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Banyuwangi\FourDay;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class EksoBanController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        return view('pages.paket.tour-banyuwangi.4D3M.EksotiskaBanyuwangi');
+    }
 }
