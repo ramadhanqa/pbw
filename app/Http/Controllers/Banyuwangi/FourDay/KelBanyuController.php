7580ad78d844d26111462f436f1652ca744c439b
Muhammad Hasbunallah Amril Sobri <ssobri55@yahoo.com>
2020-07-01 17:31:12 +0700

    Update Laravel Keliling Banyuwangi 2

--- app/Http/Controllers/Banyuwangi/FourDay/KelBanyuController.php
+++ app/Http/Controllers/Banyuwangi/FourDay/KelBanyuController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Banyuwangi\FourDay;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Testimonial;
 
 class KelBanyuController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.paket.tour-banyuwangi.4D3M.KelilingBAnyuwangi2');
+        $testimonials = Testimonial::all();
+        return view('pages.paket.tour-banyuwangi.4D3M.KelilingBAnyuwangi2',['testimonials' => $testimonials]);
     }
 }
