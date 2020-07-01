1569d884147412a92bb827aec4924757a0754ade
Muhammad Hasbunallah Amril Sobri <ssobri55@yahoo.com>
2020-07-01 16:34:51 +0700

    Update Laravel Trekking Kawah Ijen

--- app/Http/Controllers/Banyuwangi/OneDay/TrekIjenController.php
+++ app/Http/Controllers/Banyuwangi/OneDay/TrekIjenController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Banyuwangi\OneDay;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Testimonial;
 
 class TrekIjenController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.paket.tour-banyuwangi.1D.TrekIjen');
+        $testimonials = Testimonial::all();
+        return view('pages.paket.tour-banyuwangi.1D.TrekIjen',['testimonials' => $testimonials]);
     }
 }
