7aa446e82dd5e6c74399eeced90618e63eb21421
ramadhanqa <rafiqori@gmail.com>
2020-07-01 23:50:18 +0700

    reset

--- resources/views/includes/admin/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-      <div class="copyright text-center my-auto">
-        <span>Copyright &copy; Jendela Wisata 2020 - PBW A</span>
-      </div>
-    </div>
-  </footer>
