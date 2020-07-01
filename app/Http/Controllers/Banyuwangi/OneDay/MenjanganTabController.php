c57b6ac7794a9b7b1aabf4a305092973dc50c675
Muhammad Hasbunallah Amril Sobri <ssobri55@yahoo.com>
2020-07-01 16:28:12 +0700

    Update Laravel Menjangan&Tabuhan

--- app/Http/Controllers/Banyuwangi/OneDay/MenjanganTabController.php
+++ app/Http/Controllers/Banyuwangi/OneDay/MenjanganTabController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Banyuwangi\OneDay;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Testimonial;
 
 class MenjanganTabController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.paket.tour-banyuwangi.1D.Menjangan&Tabuhan');
+        $testimonials = Testimonial::all();
+        return view('pages.paket.tour-banyuwangi.1D.Menjangan&Tabuhan',['testimonials' => $testimonials]);
+        
     }
 }
